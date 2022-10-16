2cae4230fde484fef7622fb70f9fb9f447d0e837
JamieLoLo <jamielo0320@gmail.com>
2022-10-16 12:25:45 +0800

    Optimize for javascript standard style

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const app = express();
-const exphbs = require("express-handlebars");
-const port = 3000;
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const port = 3000
 
-app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-app.use(express.static("public"));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/portfolio", (req, res) => {
-  res.render("portfolio");
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on localhost:${port}`);
-});
+  console.log(`Server is running on localhost:${port}`)
+})
